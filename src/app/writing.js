c38bcaa87cd23f66fe6e92d40851ecae904fb711
unknown <robermiranda53@gmail.com>
2025-12-03 15:32:45 -0500

    d

--- src/app/writing.js
+++ src/app/writing.js
@@ -123,7 +123,7 @@ export const handleWritingCorrection = async (sentence, feedbackContainer, feedb
         };
 
         const apiKey = "AIzaSyDwazLN5ZXY61X8m3s8eMtt4AqBJSBjPVw";
-        const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-05-20:generateContent?key=${apiKey}`;
+        const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`;
 
         const response = await fetchWithRetry(apiUrl, {
             method: 'POST',
